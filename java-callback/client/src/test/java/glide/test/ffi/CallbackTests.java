0c1793f168d96e777d797ca1dc75e0154ce0a498
honhimW <honhimw@outlook.com>
2025-02-11 18:00:02 +0800

    update

--- java-callback/client/src/test/java/glide/test/ffi/CallbackTests.java
+++ java-callback/client/src/test/java/glide/test/ffi/CallbackTests.java
@@ -28,7 +28,7 @@ public class CallbackTests {
     @Test
     @SneakyThrows
     void connected() {
-        ValkeyClient client = new ValkeyClient("redis://:123456@127.0.0.1:6379");
+        ValkeyClient client = new ValkeyClient("redis://127.0.0.1:6379");
         RsLogger.init();
         CompletableFuture<String> await = new CompletableFuture<>();
         ThreadCallback.connect(client, (ConnectHandler) () -> {
