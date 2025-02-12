4574648f3b085668d13edec3d1b4193e790fe31d
honhimW <honhimw@outlook.com>
2025-02-12 15:50:48 +0800

    Revert "update"
    This reverts commit 80990b4b6ee9a208a93366265f7b5fb9b78d685c.

--- java-callback/benchmarks/src/main/java/glide/benchmarks/clients/callback/ValkeyAsyncClient.java
+++ java-callback/benchmarks/src/main/java/glide/benchmarks/clients/callback/ValkeyAsyncClient.java
@@ -3,10 +3,7 @@ package glide.benchmarks.clients.callback;
 import glide.api.ValkeyOperator;
 import glide.benchmarks.clients.AsyncClient;
 import glide.benchmarks.utils.ConnectionSettings;
-
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -16,49 +13,32 @@ import java.util.concurrent.TimeUnit;
  */
 public class ValkeyAsyncClient implements AsyncClient<String> {
 
-    private int idx = 0;
-
-    private final List<ValkeyOperator> valkeyOperators = new CopyOnWriteArrayList<>();
-
-    private ValkeyOperator next() {
-        ValkeyOperator operator;
-        int index = idx;
-        if (index < 32) {
-            operator = valkeyOperators.get(index);
-        } else {
-            operator = valkeyOperators.get(0);
-        }
-        idx++;
-        return operator;
-    }
+    private ValkeyOperator valkeyOperator;
 
     @Override
     public Future<String> asyncSet(String key, String value) {
-        return next().set(key, value);
+        return valkeyOperator.set(key, value);
     }
 
     @Override
     public Future<String> asyncGet(String key) {
-        return next().get(key);
+        return valkeyOperator.get(key);
     }
 
     @Override
     public void connectToValkey(ConnectionSettings connectionSettings) {
         String url =
-            String.format(
-                "%s://%s:%d",
-                connectionSettings.useSsl ? "rediss" : "redis",
-                connectionSettings.host,
-                connectionSettings.port);
-        for (int i = 0; i < 32; i++) {
-            ValkeyOperator operator = ValkeyOperator.fromUrl(url);
-            CompletableFuture<Void> start = operator.start();
-            try {
-                start.get(1, TimeUnit.SECONDS);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            valkeyOperators.add(operator);
+                String.format(
+                        "%s://%s:%d",
+                        connectionSettings.useSsl ? "rediss" : "redis",
+                        connectionSettings.host,
+                        connectionSettings.port);
+        valkeyOperator = ValkeyOperator.fromUrl(url);
+        CompletableFuture<Void> start = valkeyOperator.start();
+        try {
+            start.get(1, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
